127c1cce21d7b7d1040a1cbf40c5911e06338794
Gülin <yardimoglugulin@gmail.com>
2023-07-24 17:30:36 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean shouldWakeUp(boolean hav, int time) {
+        if (time<0 || time>23){
+            return false;
+        }else if (!hav) return false;
+
+    }
+
 }
